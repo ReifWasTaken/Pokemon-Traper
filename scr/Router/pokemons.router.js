0689651d3da492e5c8fdcce56a4673451acc6854
ReifWasTaken <gregodelgado182@gmail.com>
2024-10-29 15:46:42 -0300

    routers added

---
+++ scr/Router/pokemons.router.js
@@ -0,0 +1,104 @@
+import express from "express";
+const pokemonsRouter = express.Router();
+
+//Pokemons persistans 
+let pokemones = [{ id: 0, nombre: "balbasaur", element: "plant" },
+{ id: 13, nombre: "ratata", element: "normal" }]
+
+pokemonsRouter.get('/', (req, res) => {
+    res.json({
+        status: "succes",
+        message: "Pokemon list",
+        data: pokemones,
+    });
+});
+
+pokemonsRouter.get('/:id', (req, res) => {
+    const reqID = req.params.id
+    const IdFound = pokemones.find((p) => p.id == reqID);
+
+    if (IdFound) {
+        return res.status(200).json({
+            status: "succes",
+            message: "Pokemon requested Found!",
+            data: IdFound
+        });
+    } else {
+        return res.status(400).json({
+            status: "Error",
+            message: "Pokemon not found " + reqID
+        });
+    }
+});
+
+pokemonsRouter.post("/", (req, res) => {
+
+    const pokemonSend = req.body;
+    const idSend = pokemonSend.id;
+    const pokemonFound = pokemones.find((p) => p.id == idSend);
+
+    if (!pokemonFound) {
+        pokemones.push({ ...pokemonSend });
+        return res.status(201).json({ 
+            status: "succes",
+            massage: "Pokemon added",
+            data: pokemonSend
+        });
+
+    } else {
+        return res.status(400).json({ 
+            status: "error",
+            massage: "Pokemon already exist"
+        });
+    }
+
+});
+
+pokemonsRouter.put("/:id", (req, res) => {
+
+    const pokeREQ = req.body;
+
+    if (pokeREQ.id) {
+        return res.status(400).json({ 
+            status: "error",
+            message: "Wrong data" });
+    }
+
+    const pokeREQid = req.params.id;
+    const pokeFound = pokemones.findIndex((p) => p.id == pokeREQid);
+
+    if (pokeFound) {
+        pokemones[pokeFound] = { id: pokemones[pokeFound].id, ...pokeREQ }
+        return res.status(200).json({ 
+            status: "succes",
+            message: "Pokemon updated",
+            data: pokeREQ
+        });
+    } else {
+        return res.status(400).json({
+            status: "error",
+            message: "Pokemon not found"
+        });
+    }
+});
+
+pokemonsRouter.delete('/:id', (req, res) => {
+    const reqID = req.params.id
+    const IdFound = pokemones.findIndex((p) => p.id == reqID);
+
+    if (IdFound == -1) {
+        return res.status(400).json({
+            status: "error",
+            message: "Pokemon Not Found"
+        });
+    }
+    pokemones = pokemones.filter((p) => p.id != reqID);
+    return res.status(200).json({
+        status: "succes",
+        message: "pokemon deleted"
+    });
+
+});
+
+
+export default pokemonsRouter;
--- scr/main.js
+++ scr/main.js
@@ -1,88 +1,23 @@
 import express from "express";
-const app = express()
+import pokemonsRouter from "./Router/pokemons.router.js";
 
+const app = express()
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
-
 const port = 3000;
-
-
 app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}`)
 })
 
-let pokemones = [{id: 0, nombre: "balbasaur", element: "plant"},
-  {id: 13, nombre: "ratata", element: "normal"}]
-  
-
 app.get('/', (req, res) => {
   res.json('Hello PokeWorld!');
-})
-
-app.get('/pokemons', (req, res) => {
-    res.json(pokemones);
-  })
-
-app.get('/pokemons/:id', (req, res) => {
-  const reqID = req.params.id
-  const IdFound = pokemones.find((p) => p.id == reqID);
-    
-  if(IdFound){
-    return res.status(200).json(IdFound);
-   }else{
-  return res.status(400).json({error: "Pokemon not found " + reqID});
-   }
-  })
-
-app.post("/pokemons", (req, res)=> {
+});
   
-  const pokemonSend = req.body;
-  const idSend = pokemonSend.id;
-  const pokemonFound = pokemones.find((p) => p.id == idSend);
+app.use("/pokemons", pokemonsRouter);
 
-  if(!pokemonFound){
-    pokemones.push({...pokemonSend});
-    return res.status(201).json({massage: "Pokemon added"});
-    
-  }else{
-    return res.status(400).json ({massage: "Pokemon already exist"});
-  }
-
-})
-
-app.put("/pokemons/:id", (req, res) =>{
-
-  const pokeREQ = req.body;
-
-  if (pokeREQ.id){
-    return res.status(400).json({ message: "Wrong data" });
-  }
-
-  const pokeREQid = req.params.id;
-  const pokeFound = pokemones.findIndex((p) => p.id == pokeREQid);
-
-  if(pokeFound){
-    pokemones[pokeFound] = { id: pokemones[pokeFound].id, ...pokeREQ}
-    return res.status(200).json({message: "Pokemon updated"});
-  }else{
-    return res.status(400).json({ message: "Pokemon not found"});
-  }
-})
-
-app.delete('/pokemons/:id', (req, res) => {
-  const reqID = req.params.id
-  const IdFound = pokemones.findIndex((p) => p.id == reqID);
-    
-  if(IdFound == -1){
-    return res.status(400).json({message: "Pokemon Not Found"});
-  }
-  pokemones = pokemones.filter((p) => p.id != reqID);
-  return res.status(200).json({message: "pokemon deleted"});
-
-})
 
 app.get("*", (req, res) => {
   return res.send(404).json({status: "error", message: "URL not found"});
 })
 
-//clase 8 23:06
+//clase 8 51:39
